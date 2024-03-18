979b59cec2750e83410bed0a74a5422886ae3498
Your Name <you@example.com>
2024-03-18 14:56:29 -0700

    got sub events

---
+++ src/lib/Events.svelte
@@ -0,0 +1,24 @@
+<script>
+  export let events;
+  export let profiles;
+</script>
+
+{#each events as event}
+  <div class="p-2">
+    <div>
+      <span class="font-bold">Display name</span>: {profiles[
+        event.author.pubkey
+      ] && profiles[event.author.pubkey].displayName}
+    </div>
+    <div><span class="font-bold">Content</span>: {event.content}</div>
+    <div><span class="font-bold">Created</span>: {event.created_at}</div>
+    <div><span class="font-bold">Kind</span>: {event.kind}</div>
+    {#each event.tags as tag}
+      <div><span class="font-bold">Tag</span>: {tag[0]} {tag[1]}</div>
+    {/each}
+    {console.log(event.events.length)}
+    {#if event.events.length > 0}
+      <svelte:self events={event.events || []} {profiles} />
+    {/if}
+  </div>
+{/each}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,8 @@
 <script>
   import { user, loaded, ndk } from "$lib/common";
-  let events_loaded = false;
+  import * as nip19 from "nostr-tools/nip19";
+  import Events from "$lib/Events.svelte";
+  $: events_loaded = false;
   let events = [];
   let profiles = {};
   const follows = async () => {
@@ -8,17 +10,29 @@
     const _follows = await $user.follows();
     const _af = Array.from(_follows);
     const pubKeys = _af.map((f) => f.pubkey);
-    const filter = { kinds: [1, 6, 7], authors: pubKeys, limit: 100 };
+    const filter = { kinds: [1, 6], authors: pubKeys, limit: 50 };
     console.log("grabbing events", filter);
 
     events = await $ndk.fetchEvents(filter);
-    for (const event of events) {
+    for (let event of events) {
       if (!profiles[event.author.pubkey]) {
         console.log("grabbing profile", event.author.pubkey);
         await event.author.fetchProfile();
         profiles[event.author.pubkey] = event.author.profile;
       } else {
         console.log("already have profile", event.author.profile);
+      }
+      event.events = [];
+      for (let tag of event.tags) {
+        if (tag[0] === "e") {
+          console.log("fetching", tag[1]);
+          const encoded = nip19.noteEncode(tag[1]);
+
+          $ndk.fetchEvent(encoded).then(async (ev) => {
+            console.log("got event", ev);
+            event.events.push(ev);
+          });
+        }
       }
     }
     events_loaded = true;
@@ -31,16 +45,11 @@
 </script>
 
 loaded: {$loaded}
+<button class="btn variant-filled" on:click={follows}>Feed</button>
 {#if $loaded}
   <div class="h1">Hello {$user.profile.displayName}</div>
   {#if events_loaded}
-    {#each events as event}
-      <div>
-        {event.content}
-        {event.kind}
-        {profiles[event.author.pubkey].displayName}
-      </div>
-    {/each}
+    Events loaded
+    <Events {events} {profiles} />
   {/if}
-  <button class="btn variant-filled" on:click={follows}>Feed</button>
 {/if}
