d37e472f7ddf8407eeca7232e92cf8c8c4486a27
Your Name <you@example.com>
2024-04-21 12:17:00 -0700

    replace npub

--- src/lib/Thread.svelte
+++ src/lib/Thread.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
   import { newThread } from "$lib/Thread";
-  import type { NRelay, NostrEvent } from "@nostrify/nostrify";
+  import type { NRelay, NostrEvent, NostrMetadata } from "@nostrify/nostrify";
   import type { Thread } from "$lib/Thread";
+  import * as nip19 from "nostr-tools/nip19";
+  import { NSchema as n } from "@nostrify/nostrify";
+
   export let event: NostrEvent;
   export let relay: NRelay;
   let thread: Thread;
@@ -12,11 +15,20 @@
 </script>
 
 {#if thread && thread.parent_item.author}
-  <div>
+  <div class="mb-10">
+    {#each thread.children as child}
+      {#if child.author}
+        <div>
+          <a href="https://primal.net/e/{child.event.id}"
+            >{child.author.display_name || child.author.name}</a
+          >: {child.body}
+        </div>
+      {/if}
+    {/each}
     <a href="https://primal.net/e/{thread.parent_item.event.id}"
       >{thread.parent_item.author.display_name ||
         thread.parent_item.author.name}</a
-    >: {thread.parent_item.event.content}
+    >: {thread.parent_item.body}
     {#each thread.parent_item.event.tags as tag}
       <div>
         [{tag[0]}]
@@ -24,24 +36,6 @@
       </div>
       {#if tag[0] == "r"}
         <img src={tag[1]} class="max-w-60" />
-      {/if}
-    {/each}
-    {#each thread.children as child}
-      {#if child.author}
-        <div class="p-2">
-          <a href="https://primal.net/e/{child.event.id}"
-            >{child.author.display_name || child.author.name}</a
-          >: {child.event.content}
-        </div>
-        {#each child.event.tags as tag}
-          <div class="p-2">
-            [{tag[0]}]
-            <a href="https://primal.net/{tag[0]}/{tag[1]}"> {tag[1]}</a>
-          </div>
-          {#if tag[0] == "r"}
-            <img src={tag[1]} class="max-w-60" />
-          {/if}
-        {/each}
       {/if}
     {/each}
   </div>
--- src/lib/Thread.ts
+++ src/lib/Thread.ts
@@ -1,5 +1,7 @@
 import type { NRelay, NostrEvent, NostrMetadata } from "@nostrify/nostrify";
 import { NSchema as n } from '@nostrify/nostrify';
+import * as nip19 from "nostr-tools/nip19";
+
 
 export interface Thread {
     parent_item: FeedItem
@@ -9,6 +11,22 @@ export interface Thread {
 interface FeedItem {
     event: NostrEvent
     author: NostrMetadata
+    body: string
+}
+
+async function replaceNpub(text: string, relay: NRelay) {
+    const match = text.match(/nostr:npub\w*/);
+    let npub: string = "";
+    let author: NostrMetadata = {};
+    if (match) {
+        npub = match[0];
+        const events = await relay.query([{ kinds: [0], authors: [npub] }]);
+        if (events) {
+            author = n.json().pipe(n.metadata()).parse(events[0].content);
+        }
+    }
+    const name = author.name || npub;
+    return text.replace(/nostr:npub\w*/, name);
 }
 
 async function grabItem(eventId: string, relay: NRelay): Promise<FeedItem> {
@@ -17,7 +35,7 @@ async function grabItem(eventId: string, relay: NRelay): Promise<FeedItem> {
         const author_events = await relay.query([{ kinds: [0], authors: [events[0].pubkey] }])
         if (author_events.length) {
             let author = n.json().pipe(n.metadata()).parse(author_events[0].content);
-            return { event: events[0], author }
+            return { event: events[0], author, body: await replaceNpub(events[0].content, relay) }
 
         }
     }
